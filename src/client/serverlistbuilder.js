4d3949f7779af67fd1c89bfd8303f226559c28e9
Evan Pratten <ewpratten@gmail.com>
2020-04-09 13:02:14 -0400

    add colors to bars

--- src/client/serverlistbuilder.js
+++ src/client/serverlistbuilder.js
@@ -3,7 +3,18 @@
  */
 
 export function buildHTMLForServer(server, fillage) {
-    return `<tr><td><a href="" onclick="connectToServer(\'${server}\');"><div class="progress"><div class="progress-bar" role="progressbar" style="width: ${fillage}%" aria-valuenow="${fillage}" aria-valuemin="0" aria-valuemax="100">${server}</div></div></a></td></tr>`;
+    // Determine bar color
+    let bar_color = "success";
+    if (fillage > 25 && fillage <= 50) {
+        bar_color = "info";
+    } else if (fillage > 50 && fillage <= 90) {
+        bar_color = "warning";
+    } else if (fillage > 90) {
+        bar_color = "danger";
+    }
+
+    // Build the HTML
+    return `<tr><td><a href="" onclick="connectToServer(\'${server}\');"><div class="progress"><div class="progress-bar bg-${bar_color}" role="progressbar" style="width: ${fillage}%" aria-valuenow="${fillage}" aria-valuemin="0" aria-valuemax="100">${server}</div></div></a></td></tr>`;
 }
 
 let serversList = document.getElementById("gen-servers");
