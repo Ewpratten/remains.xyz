ca778a3ede941c44f68c1bff73902c4d3804d3ac
Evan Pratten <ewpratten@gmail.com>
2020-04-10 18:17:43 -0400

    jank, yet cool, AI

--- src/server/botplayer.js
+++ src/server/botplayer.js
@@ -7,27 +7,25 @@ class BotPlayer extends player.Player {
         // Handle death marking
         if (this.health <= 0) {
             this.alive = false;
-        }3
+        }
+
+        // Set the time alive
+        this.aliveTime = Math.round((new Date().getTime() / 1000) - this.spawnTime);
 
         // Get distance to closest player
         let closestDist = 1000000000;
-        let closest = { x: null, y: null };
+        let closest = { x: 1, y: 0 };
         for (let i = 0; i < this.server.players.length; i++) {
 
             // Find distance to player
             let dist = Math.abs(Math.hypot(this.x - this.server.players[i].x, this.y - this.server.players[i].y));
 
             // If this is closer, set it
-            if (dist < closestDist) {
+            if (dist < closestDist && dist != 0) {
                 closest.x = this.x - this.server.players[i].x;
                 closest.y = this.y - this.server.players[i].y;
                 closestDist = dist;
             }
-        }
-
-        // Only auto-target if there is a target
-        if (closest.x == null) {
-            return;
         }
 
         // Move to the closest player
@@ -40,6 +38,7 @@ class BotPlayer extends player.Player {
         // Clamp speed
         this.dx = Math.max(Math.min(closest.x, this.speed), -this.speed);
         this.dy = Math.max(Math.min(closest.y, this.speed), -this.speed);
+
 
         // Handle movement
         let newX = this.x + this.dx;
